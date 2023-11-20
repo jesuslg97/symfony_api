e53eaa98d9fa03b3bed1c5fab158ea52f3e7f001
jesuslg97 <jelopezgonzalez@gmail.com>
2023-11-20 12:59:09 +0000

    add templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -18,9 +19,16 @@ class QuestionController
      * @Route("/questions/{slug}")
      */
     public function show($slug) {
-        return new Response(sprintf(
-            'vamos vamos "%s"',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+
+        $answers = [
+            'hola1',
+            'hola2',
+            'hola3',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
