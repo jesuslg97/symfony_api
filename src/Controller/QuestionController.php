20221e5004c43a3d028edd9e5faf212f8cf382bc
jesuslg97 <jelopezgonzalez@gmail.com>
2023-11-23 11:59:04 +0000

    using Service without inyeccion de dependencia

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+
+    private $logger;
+    private $isDebug;
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -28,7 +37,11 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache) {
+    public function show($slug, MarkdownHelper $markdownHelper) {
+
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
 
         $answers = [
             '`hola1`',
@@ -38,9 +51,7 @@ class QuestionController extends AbstractController
 
         $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
-        $parseQuestionText = $cache->get('markdown_'.md5($questionText), function () use ($markdownParser, $questionText){
-            return $markdownParser->transformMarkdown($questionText);
-        });
+        $parseQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
