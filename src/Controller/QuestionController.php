b03e01505fb3e86b892b147caa7035243919de00
jesuslg97 <jelopezgonzalez@gmail.com>
2023-11-30 12:14:32 +0000

    add colum votes, add function up or down votes in questions ask

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -56,6 +57,7 @@ EOF
         if (rand(1, 10) > 2) {
             $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
+        $question->setVotes(rand(-20, 50));
 
         $entityManager->persist($question);
         $entityManager->flush();
@@ -84,6 +86,25 @@ EOF
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug()
         ]);
     }
 
