3ebda26fb2a92a62ece70a4bb8772b7477ff7b37
jesuslg97 <jelopezgonzalez@gmail.com>
2023-11-30 11:01:47 +0000

    changes in method new and show, homepage dinamic, using info DB

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,21 +26,50 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(Environment $twigEnviroment) {
+    public function homepage(QuestionRepository $repository) {
 
-        /*
-        $html = $twigEnviroment->render('question/homepage.html.twig');
+        $questions = $repository->findAllAskedOrderedByNewest();
 
-        return new Response($html);
-        */
+        return $this->render('question/homepage.html.twig', [
+            'questions' => $questions,
+        ]);
+    }
 
-        return $this->render('question/homepage.html.twig');
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager) {
+
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
     }
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper) {
+    public function show(Question $question) {
 
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
@@ -49,14 +81,10 @@ class QuestionController extends AbstractController
             'hola3',
         ];
 
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-
-        $parseQuestionText = $markdownHelper->parse($questionText);
-
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $parseQuestionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
+
 }
