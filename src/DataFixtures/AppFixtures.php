7756c3925eb4a4d89b6366fda57ec6a3ab81a22a
jesuslg97 <jelopezgonzalez@gmail.com>
2023-11-30 12:42:06 +0000

    install packages fixtures and zenstruck_foundry

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Question;
+use App\Factory\QuestionFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        QuestionFactory::new()->createMany(6);
+
+        $manager->flush();
+    }
+}
