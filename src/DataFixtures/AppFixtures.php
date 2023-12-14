8834940cd794da5ded716608bae3dec1f214fdf0
jesuslg97 <jelopezgonzalez@gmail.com>
2023-12-14 12:35:24 +0000

    create answer factory and using relation between question

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,24 +13,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        QuestionFactory::new()->createMany(6);
+        $questions = QuestionFactory::createMany(6);
 
         QuestionFactory::new()
             ->unpublished()
             ->createMany(2);
 
-        $answer = new Answer();
-        $answer->setContent('This question is the best? I wish... I knew the answer.');
-        $answer->setUsername('weaverryan');
-
-        $question = new Question();
-        $question->setName('How to un-disappear your wallet.');
-        $question->setQuestion('... I should not have done this...');
-
-        $answer->setQuestion($question);
-        
-        $manager->persist($answer);
-        $manager->persist($question);
+        AnswerFactory::createMany(100, function() use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)],
+            ];
+        });
 
         $manager->flush();
 
