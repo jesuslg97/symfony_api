fea360b9fe95884e9947ad01b678839ab3cbfbf9
jesuslg97 <jelopezgonzalez@gmail.com>
2023-12-14 11:58:04 +0000

    create table Answer

---
+++ migrations/Version20231214112457.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231214112457 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE answer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE answer (id INT NOT NULL, content TEXT NOT NULL, username VARCHAR(255) NOT NULL, votes INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE question ALTER created_at SET NOT NULL');
+        $this->addSql('ALTER TABLE question ALTER updated_at SET NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE answer_id_seq CASCADE');
+        $this->addSql('DROP TABLE answer');
+        $this->addSql('ALTER TABLE question ALTER created_at DROP NOT NULL');
+        $this->addSql('ALTER TABLE question ALTER updated_at DROP NOT NULL');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -16,5 +17,21 @@ class AppFixtures extends Fixture
         QuestionFactory::new()
             ->unpublished()
             ->createMany(2);
+
+        $answer = new Answer();
+        $answer->setContent('This question is the best? I wish... I knew the answer.');
+        $answer->setUsername('weaverryan');
+
+        $question = new Question();
+        $question->setName('How to un-disappear your wallet.');
+        $question->setQuestion('... I should not have done this...');
+
+        $answer->setQuestion($question);
+        
+        $manager->persist($answer);
+        $manager->persist($question);
+
+        $manager->flush();
+
     }
 }
