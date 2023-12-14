df79b1bc19009095e57b15b462b122a6ab1081b4
jesuslg97 <jelopezgonzalez@gmail.com>
2023-12-14 11:16:35 +0000

    using fields created_at and update_at

--- src/Factory/QuestionFactory.php
+++ src/Factory/QuestionFactory.php
@@ -58,12 +58,7 @@ final class QuestionFactory extends ModelFactory
     {
         // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
         return $this
-            ->afterInstantiate(function(Question $question): void {
-                if (!$question->getSlug()) {
-                    $slugger = new AsciiSlugger();
-                    $question->setSlug($slugger->slug($question->getName()));
-                }
-            })
+
         ;
     }
 
